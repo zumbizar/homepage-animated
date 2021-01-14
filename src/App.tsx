53a0a84e9e735d99238300949ebe3ccdffd5a638
Fernando Henrique <68034298+zumbizar@users.noreply.github.com>
2021-01-14 16:50:15 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import GlobalStyles from "./styles/global";
+import { Main, Section, Sticky } from "./styles";
+import FirstAndSecond from "./components/FirstAndSecond";
 
 function App() {
+  React.useEffect(() => {
+    window.scrollTo({ top: 0 });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyles />
+
+      <Main style={{ height: "1610vh" }}>
+        <Section style={{ height: "72.7%" }}>
+          <FirstAndSecond />
+        </Section>
+
+        <Section style={{ height: "9.7%" }}>
+          <Sticky className="Third" />
+        </Section>
+
+        <Section style={{ height: "10.1%" }}>
+          <Sticky className="Fourth" />
+        </Section>
+      </Main>
+    </>
   );
 }
 
---
+++ src/components/FirstAndSecond/index.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { Sticky } from "../../styles";
+import { motion, useTransform, useViewportScroll } from "framer-motion";
+
+const FirstAndSecond: React.FC = () => {
+  const { scrollYProgress } = useViewportScroll();
+
+  const frameOpacity = useTransform(scrollYProgress, [0.196, 0.198], [0, 1]);
+  const frameScale = useTransform(
+    scrollYProgress,
+    [0.558, 0.627],
+    [0.511, 0.8]
+  );
+
+  return (
+    <Sticky className="Second">
+      <First />
+
+      <motion.div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: "100vh",
+          borderRadius: "4px",
+          border: "4px solid #fff",
+          opacity: frameOpacity,
+          scale: frameScale,
+        }}
+      />
+    </Sticky>
+  );
+};
+
+const First: React.FC = () => {
+  const { scrollYProgress } = useViewportScroll();
+
+  const firstScale = useTransform(
+    scrollYProgress,
+    [0.198, 0.264, 0.558, 0.627],
+    [1, 0.511, 0.511, 1]
+  );
+  const firstRadius = useTransform(
+    scrollYProgress,
+    [0.198, 0.264, 0.558, 0.627],
+    [0, 4, 4, 0]
+  );
+  const leftSideHight = useTransform(
+    scrollYProgress,
+    [0, 0.0558],
+    ["20vh", "100vh"]
+  );
+  const rightSideScale = useTransform(
+    scrollYProgress,
+    [0.047, 0.093],
+    [0, 0.511]
+  );
+  const rightSideY = useTransform(
+    scrollYProgress,
+    [0.047, 0.093],
+    ["58vh", "0vh"]
+  );
+
+  return (
+    <Sticky
+      className="First"
+      style={{ scale: firstScale, borderRadius: firstRadius }}
+    >
+      <div className="a">
+        <motion.div className="left-side" style={{ height: leftSideHight }} />
+
+        <div className="right-side">
+          <motion.div
+            className="right-image"
+            style={{ y: rightSideY, scale: rightSideScale }}
+          />
+        </div>
+      </div>
+      <div className="b"></div>
+      <div className="c"></div>
+    </Sticky>
+  );
+};
+
+export default FirstAndSecond;
